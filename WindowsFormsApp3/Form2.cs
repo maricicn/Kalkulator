2b0d1daf321150fb1988b3ab46202502862cd074
Admin <Admin@DESKTOP-KKLBH2A>
2024-01-28 11:18:28 +0100

    Add project files.

---
+++ WindowsFormsApp3/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public static Form2 form2instance;
+        public Form2()
+        {
+            InitializeComponent();
+            form2instance = this;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
